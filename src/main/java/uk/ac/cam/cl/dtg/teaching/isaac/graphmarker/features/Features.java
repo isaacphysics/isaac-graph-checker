c716d8d56419c53f1390abaeba21d3cbbdd969ae
Robin Message <robin@lambdacambridge.com>
2019-03-01 11:36:07 +0000

    Added debug logging of non-matching features

--- src/main/java/uk/ac/cam/cl/dtg/teaching/isaac/graphmarker/features/Features.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/isaac/graphmarker/features/Features.java
@@ -1,16 +1,23 @@
 package uk.ac.cam.cl.dtg.teaching.isaac.graphmarker.features;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Streams;
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.ac.cam.cl.dtg.teaching.isaac.graphmarker.data.Input;
 import uk.ac.cam.cl.dtg.teaching.isaac.graphmarker.data.Line;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Features {
+
+    private static final Logger log = LoggerFactory.getLogger(Features.class);
+
     private static List<LineFeature> lineFeatures = ImmutableList.of(
         new ExpectedSectorsFeature(),
         new SlopeFeature(),
@@ -32,16 +39,30 @@ public class Features {
                 .map(item -> itemToFeaturePredicate(item))
                 .collect(Collectors.toList());
 
-        List<Predicate<Input>> matchers = matchersAndInfo.stream()
-            .map(pair -> pair.left)
-            .collect(Collectors.toList());
+        List<ImmutablePair<Predicate<Input>, String>> matchers = Streams.zip(matchersAndInfo.stream(),
+            Arrays.stream(features), (pair, name) -> ImmutablePair.of(pair.left, name)
+        ).collect(Collectors.toList());
 
-        if (matchersAndInfo.stream()
-            .noneMatch(pair -> pair.right)) {
-            matchers.add(curvesCountFeature.matcher(curvesCountFeature.deserialize("1")));
+        if (matchersAndInfo.stream().noneMatch(pair -> pair.right)) {
+            matchers.add(ImmutablePair.of(curvesCountFeature.matcher(curvesCountFeature.deserialize("1")),
+                "curves: 1 (implicit)"));
         }
 
-        return input -> matchers.stream().allMatch(matcher -> matcher.test(input));
+        return input -> {
+            List<String> failedPredicates = new ArrayList<>();
+            boolean success = true;
+            for (ImmutablePair<Predicate<Input>, String> matcher: matchers) {
+                boolean test = matcher.left.test(input);
+                success &= test;
+                if (!test) {
+                    failedPredicates.add(matcher.right);
+                }
+            }
+            if (!success) {
+                log.info("Failed predicates: " + String.join("; ", failedPredicates));
+            }
+            return success;
+        };
     }
 
     private static ImmutablePair<Predicate<Input>, Boolean> itemToFeaturePredicate(String item) {
